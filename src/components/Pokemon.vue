f3090896ef997b51331455272954a7b3dbb7a989
David Gallego Gómez <dgallego@dgallego.ETS.ES>
2019-12-10 12:42:41 +0100

    feat: basic project finished

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="pokemon">
+    <div class="pokemon-head"></div>
+    <div class="pokemon-body"></div>
+    <div class="pokemon-feet"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pokemon"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.pokemon {
+  width: 3rem;
+  display: inline-block;
+  margin: 1rem;
+}
+.pokemon-head,
+.pokemon-body {
+  height: 3rem;
+}
+.pokemon-feet {
+  height: 1rem;
+}
+
+.pokemon.bulbasaur .pokemon-head {
+  background-color: #ff6a62;
+}
+.pokemon.bulbasaur .pokemon-body {
+  background-color: #62d5b4;
+}
+.pokemon.bulbasaur .pokemon-feet {
+  background-color: #317373;
+}
+
+.pokemon.squirtle .pokemon-head,
+.pokemon.squirtle .pokemon-body {
+  background-color: #8bc5cd;
+}
+.pokemon.squirtle .pokemon-feet {
+  background-color: #ffe69c;
+}
+
+.pokemon.charmander {
+  background-color: #de5239;
+}
+
+.pokemon.picachu {
+  background-color: #f6e652;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="actions-container">
+      <button @click="fight">FIGHT</button>
+    </div>
+
+    <div :class="['box', { winner: player1.winner }]">
+      <select v-model="player1.pokemon" @change="resetWinner">
+        <option
+          v-for="pokemon in pokemons"
+          :value="pokemon"
+          :key="pokemon.id"
+          >{{ pokemon.name.toUpperCase() }}</option
+        >
+      </select>
+      <pokemon :class="player1.pokemon.name"></pokemon>
+    </div>
+
+    <label>VS</label>
+
+    <div :class="['box', { winner: player2.winner }]">
+      <select v-model="player2.pokemon" @change="resetWinner">
+        <option
+          v-for="pokemon in pokemons"
+          :value="pokemon"
+          :key="pokemon.id"
+          >{{ pokemon.name.toUpperCase() }}</option
+        >
+      </select>
+      <pokemon :class="player2.pokemon.name"></pokemon>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Pokemon from "@/components/Pokemon.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data: () => ({
+    player1: { pokemon: {}, winner: false },
+    player2: { pokemon: {}, winner: false },
+    pokemons: [
+      { id: 0, name: "picachu", type: "electro" },
+      { id: 1, name: "bulbasaur", type: "planta" },
+      { id: 2, name: "squirtle", type: "agua" },
+      { id: 3, name: "charmander", type: "fuego" }
+    ],
+    results: [
+      [0, 2, 1, 0],
+      [1, 0, 2, 2],
+      [2, 1, 0, 1],
+      [0, 1, 2, 0]
+    ]
+  }),
+  methods: {
+    fight: function() {
+      const result = this.results[this.player1.pokemon.id][
+        this.player2.pokemon.id
+      ];
+      const selectWinner = [
+        () => {
+          (this.player1.winner = true), (this.player2.winner = true);
+        }, // empate
+        () => {
+          (this.player1.winner = true), (this.player2.winner = false);
+        }, // gana player1
+        () => {
+          (this.player1.winner = false), (this.player2.winner = true);
+        } // gana player2
+      ];
+      selectWinner[result]();
+    },
+    resetWinner: function() {
+      (this.player1.winner = false), (this.player2.winner = false);
+    }
   }
 };
 </script>
+
+<style scoped lang="scss">
+.box {
+  display: inline-block;
+  padding: 1rem;
+  margin: 1rem;
+}
+.box.winner {
+  @extend .box;
+  background-color: green;
+}
+</style>
